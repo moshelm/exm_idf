9afda70907f43e82b11180fb887c9b7319742da0
Moshe <moshe@example.com>
2025-11-28 09:38:18 +0200

    CRT fix and change logic functions

--- placement_logic.py
+++ placement_logic.py
@@ -2,40 +2,27 @@ from obgects.solidrs import Solider
 from obgects.base import Base
 
 class ControlUnit:
-    def __init__(self, info:list[Solider]):
-        self.soldiers = info
+    def __init__(self):
+        self.base = Base()
+        self.soldiers : list[Solider] = []
         self.waiting_list = []
         self.num_placements = 0
 
-    def not_placed_yet(self):
-        return [soldier for soldier in self.soldiers if not soldier.is_placed]
+    def new_group_to_place(self,group):
+        self.soldiers = group
 
-    def placement_soldiers(self,base:Base):
-        while True:
-            farthest = self.find_farthest()
-            for sol in self.soldiers:
+    def find(self):
+        self.soldiers.sort(reverse=True,key=lambda sol:sol.distance_base)
 
-                if farthest:
-                    if farthest == sol:
-                        if not base.is_base_full():
-                            base.add_tenant(farthest)
-                            self.num_placements += 1
-                            sol.is_placed = True
-                        else:
-                            self.waiting_list.extend(self.soldiers)
-                            return self.soldiers
-                else:
-                    return self.soldiers
+    def do_it(self):
+        self.find()
+        for sol in self.soldiers:
+            if not self.base.is_base_full():
+                self.base.add_tenant(sol)
+                sol.is_placed = True
+                self.num_placements += 1
+            else:
+                self.waiting_list.append(sol)
 
 
-    def find_farthest(self):
-        if self.soldiers:
-            farthest_location = max([sol.distance_base for sol in self.soldiers if not sol.is_placed])
-            farthest = None
-            for soldier in self.soldiers:
-                if soldier.distance_base == farthest_location :
-                    farthest = soldier
-            return farthest
-        else:
-            return None
 
