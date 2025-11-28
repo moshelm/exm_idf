5be5e381ba2692382d740af116d84a68ebabbee1
Moshe <moshe@example.com>
2025-11-28 09:39:04 +0200

    create manager for all routes

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import csv
-
 import uvicorn
-from fastapi import UploadFile, FastAPI, File, APIRouter
+from fastapi import UploadFile, FastAPI, File
 from typing import Optional
 import io
-from obgects.base import Base
-from placement_logic import *
+from obgects.solidrs import Solider
+from placement_logic import ControlUnit
 
 app = FastAPI()
-
-base = Base()
+control = ControlUnit()
 
 @app.post('/assignWithCsv')
 def upload_file(file: UploadFile ):
@@ -22,15 +20,15 @@ def upload_file(file: UploadFile ):
 
     soldiers :list[Solider] = info_soldiers_to_objects(info_rows)
 
-    report = ControlUnit(soldiers)
-    report = report.placement_soldiers(base)
+    control.new_group_to_place(soldiers)
+    control.do_it()
     response = {
-        'soldiers deployed:':report.num_placements,
-        'soldiers left:':len(report.waiting_list)
+        'soldiers deployed:':control.num_placements,
+        'soldiers left:':len(control.waiting_list)
         }
 
-    for soldier in soldiers:
-        info_report = {f'{soldier}':soldier.location}
+    for soldier in control.soldiers:
+        info_report = {f'{soldier}':soldier.location if soldier.is_placed else 'not placed'}
         response.update(info_report)
 
     return response
@@ -39,23 +37,40 @@ def upload_file(file: UploadFile ):
 
 @app.get('/space')
 def get_space_info():
-    dorms = base.dorms
-
-    return {}
+    info_rooms = control.base.get_info_rooms()
+    return {'rooms full : ':info_rooms[0],
+            'rooms empty : ':info_rooms[1],
+            'rooms middel : ':info_rooms[2]}
 
 @app.get('/search')
-def search_by_personal_number():
-    pass
+def search_by_personal_number(soldier_id:str):
+    info = {}
+    print(control.soldiers)
+    for sol in control.soldiers:
+        if int(sol.personal_number) == int(soldier_id):
+            info['soldier'] = sol.__str__()
+            if sol.is_placed:
+                info['is placed'] = 'yes'
+                dorm,room = sol.location.values()
+                info['location'] = f'dorm : {dorm} room : {room}'
+            else :
+                info['is placed'] = 'no'
+                info['status'] = 'waiting list'
+            return info
+
+    return {soldier_id: 'not in list'}
+
 @app.get('/waitingList')
 def waiting_list():
-    pass
+    return {'waiting list':[sol.__str__() for sol in control.waiting_list]}
+
 
 
 
 def info_soldiers_to_objects(info: list[list]):
     soldiers_of_file = []
     for row in info:
-        soldiers_of_file.append(Solider(row[0], row[1], row[2], row[3], row[4], row[5]))
+        soldiers_of_file.append(Solider(row[0], row[1], row[2], row[3], row[4], int(row[5])))
     return soldiers_of_file
 
 
